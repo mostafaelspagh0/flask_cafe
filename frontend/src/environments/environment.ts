bed4a55645d46ab1a002f0bdb22d7b7515b5f503
mostafa elspagh <mostafaelspagh0@gmail.com>
2021-03-08 02:22:52 +0200

    remove done todo comment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,14 +1,10 @@
-/* @TODO replace with your variables
- * ensure all variables on this page match your project
- */
-
 export const environment = {
-  production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
-  auth0: {
-    url: 'happy2000.us', // the auth0 domain prefix
-    audience: 'http://localhost/:5000', // the audience set for the auth0 app
-    clientId: 'k7ZEqOyBoPUs2zqw5xBHh1ytMrzpma67', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:4200', // the base url of the running ionic application.
-  }
+    production: false,
+    apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+    auth0: {
+        url: 'happy2000.us', // the auth0 domain prefix
+        audience: 'http://localhost/:5000', // the audience set for the auth0 app
+        clientId: 'k7ZEqOyBoPUs2zqw5xBHh1ytMrzpma67', // the client id generated for the auth0 app
+        callbackURL: 'http://localhost:4200', // the base url of the running ionic application.
+    }
 };
