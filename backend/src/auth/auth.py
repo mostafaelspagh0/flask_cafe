7bf1a773293187dc58a188c0ceb9192073c860b9
mostafa elspagh <mostafaelspagh0@gmail.com>
2021-03-08 08:46:40 +0200

    implement get token

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -34,7 +34,39 @@ class AuthError(Exception):
 
 
 def get_token_auth_header():
-    raise Exception('Not Implemented')
+    auth_header = request.headers.get("Authorization", None)
+    if auth_header is None:
+        raise AuthError(
+            {
+                "code": "authorization_header_missing",
+                "description": "Authorization header is expected"
+            }, 401)
+
+    parts = auth_header.split()
+
+    if parts[0].lower() != "bearer":
+        raise AuthError(
+            {
+                "code": "invalid_header",
+                "description": "Authorization header must start with"
+                               " Bearer"
+            }, 401)
+    elif len(parts) == 1:
+        raise AuthError(
+            {
+                "code": "invalid_header",
+                "description": "Token not found"
+            }, 401)
+    elif len(parts) > 2:
+        raise AuthError(
+            {
+                "code": "invalid_header",
+                "description": "Authorization header must be"
+                               " Bearer token"
+            }, 401)
+
+    token = parts[1]
+    return token
 
 
 '''
