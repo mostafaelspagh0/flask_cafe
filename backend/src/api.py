1718937722a24df0d81bc8e4df86cfee06b9a8ef
mostafa elspagh <mostafaelspagh0@gmail.com>
2021-03-08 09:55:08 +0200

    remove done todo comment

--- backend/src/api.py
+++ backend/src/api.py
@@ -17,26 +17,19 @@ CORS(app)
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
+
+
 # db_drop_and_create_all()
 
 ## ROUTES
-'''
-@TODO implement endpoint
-    GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
 
 
 @app.route('/drinks', methods=['GET'])
 def get_drinks():
-    drinks = Drink.query.all()
     try:
         return jsonify({
             "success": True,
-            "drinks": [drink.short() for drink in drinks]
+            "drinks": [drink.short() for drink in Drink.query.all()]
         }), 200
     except Exception:
         return jsonify({
